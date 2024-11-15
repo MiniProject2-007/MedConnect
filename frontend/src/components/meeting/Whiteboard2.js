6d3307dd8836dba89f341ab5658f6186d09a7bae
tanmay-8 <tanmayshingde8@gmail.com>
2024-11-15 21:21:15 +0530

    whiteboard

--- frontend/src/components/meeting/Whiteboard2.js
+++ frontend/src/components/meeting/Whiteboard2.js
@@ -5,7 +5,7 @@ import React from "react";
 import "tldraw/tldraw.css";
 
 const Whiteboard2 = ({ id }) => {
-    const store = useSyncDemo({ roomId: $`peer-connect-007-${id}` });
+    const store = useSyncDemo({ roomId: `peer-connect-007-${id}` });
     return (
         <div style={{ position: "fixed", inset: 0 }}>
             {" "}
